373f6c52869ab5573364d2b10a69b52ea954285e
zephyrstudiosllc <zschwendi@zephyrstudios.dev>
2026-08-24 14:42:12 -0600

    Initial Commit

---
+++ sidepuls-z-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sidepuls-z-swift
+//
+//  Created by Zach on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sidepuls-z-swift/sidepuls_z_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidepuls_z_swiftApp.swift
+//  sidepuls-z-swift
+//
+//  Created by Zach on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct sidepuls_z_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
